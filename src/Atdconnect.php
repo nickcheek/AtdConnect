8e7f14aa6f9a9d8fd4aace0e767e885ff7e9990d
Nick <nick@nicholascheek.com>
2019-02-06 10:07:33 -0600

    Fixed Spacing

--- src/Atdconnect.php
+++ src/Atdconnect.php
@@ -6,32 +6,33 @@ use SoapClient;
 
 class Atdconnect
 {
-
-	public static $wsshead;
-	
-	private static $brandwsdl='https://testws.atdconnect.com/ws/3_4/brandstyles.wsdl';
-	private static $locationwsdl = 'https://testws.atdconnect.com/ws/3_4/locations.wsdl';
-	private static $productwsdl = 'https://testws.atdconnect.com/ws/3_4/products.wsdl';
-
-   public function __construct($user,$pass,$client)
-   {
-	   self::$wsshead = $this->getWSSHeader($user,$pass,$client);
-   }
-   
-   public static function getWSSHeader($user,$pass,$client){
-	    $xml = '
+    
+    public static $wsshead;
+    
+    private static $brandwsdl = 'https://testws.atdconnect.com/ws/3_4/brandstyles.wsdl';
+    private static $locationwsdl = 'https://testws.atdconnect.com/ws/3_4/locations.wsdl';
+    private static $productwsdl = 'https://testws.atdconnect.com/ws/3_4/products.wsdl';
+    
+    public function __construct($user, $pass, $client)
+    {
+        self::$wsshead = $this->getWSSHeader($user, $pass, $client);
+    }
+    
+    public static function getWSSHeader($user, $pass, $client)
+    {
+        $xml = '
 		<wsse:Security SOAP-ENV:mustUnderstand="1" xmlns:wsse="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd">
-		    <wsse:UsernameToken atd:clientId="'.$client.'" xmlns:atd="http://api.atdconnect.com/atd">
-		        <wsse:Username>'.$user.'</wsse:Username>
-		        <wsse:Password Type="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordText">'.$pass.'</wsse:Password>
+		    <wsse:UsernameToken atd:clientId="' . $client . '" xmlns:atd="http://api.atdconnect.com/atd">
+		        <wsse:Username>' . $user . '</wsse:Username>
+		        <wsse:Password Type="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordText">' . $pass . '</wsse:Password>
 		    </wsse:UsernameToken>
 		</wsse:Security>
 		';
-		
-		return new \SoapHeader('http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd','Security',new \SoapVar($xml, XSD_ANYXML), true);
-    }   
+        
+        return new \SoapHeader('http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd', 'Security', new \SoapVar($xml, XSD_ANYXML), true);
+    }
     
-
+    
     
     //************************************************
     //               Location Service
@@ -39,82 +40,95 @@ class Atdconnect
     
     public static function getLocationByCriteria()
     {
-    	
-	    $client = new \SoapClient(self::$locationwsdl);
-		$client->__setSoapHeaders(self::$wsshead);
-		$response   = $client->getLocationByCriteria();
-		
-		return $response;
-    }    
-    
-    public static function getLocationCutoffTimes($location='1213421')
-    {
-    	
-	    $client = new \SoapClient(self::$locationwsdl);
-		$client->__setSoapHeaders(self::$wsshead);
-		$response   = $client->getLocationCutoffTimes(array('location'=>$location));
-		
-		return $response;
+        
+        $client = new \SoapClient(self::$locationwsdl);
+        $client->__setSoapHeaders(self::$wsshead);
+        $response = $client->getLocationByCriteria();
+        
+        return $response;
     }
     
-    public static function getDistributionCenter($dc='059')
+    public static function getLocationCutoffTimes($location = '1213421')
     {
-	   $client = new \SoapClient(self::$locationwsdl);
-	   $client->__setSoapHeaders(self::$wsshead);
-	   $response   = $client->getDistributionCenter(array('servicingDC'=>$dc));
-		
-	   return $response; 
+        
+        $client = new \SoapClient(self::$locationwsdl);
+        $client->__setSoapHeaders(self::$wsshead);
+        $response = $client->getLocationCutoffTimes(array(
+            'location' => $location
+        ));
+        
+        return $response;
+    }
+    
+    public static function getDistributionCenter($dc = '059')
+    {
+        $client = new \SoapClient(self::$locationwsdl);
+        $client->__setSoapHeaders(self::$wsshead);
+        $response = $client->getDistributionCenter(array(
+            'servicingDC' => $dc
+        ));
+        
+        return $response;
     }
     
     //************************************************
     //               Brand Styles Service
     //************************************************
     
-            
-    public static function getBrand($location,$product=null)
+    
+    public static function getBrand($location, $product = null)
     {
-	   $client = new \SoapClient(self::$brandwsdl);
-	   $client->__setSoapHeaders(self::$wsshead);
-	   $response   = $client->getBrand(array('locationNumber'=>$location,'productGroup'=>$product));
-		
-	   return $response; 
+        $client = new \SoapClient(self::$brandwsdl);
+        $client->__setSoapHeaders(self::$wsshead);
+        $response = $client->getBrand(array(
+            'locationNumber' => $location,
+            'productGroup' => $product
+        ));
+        
+        return $response;
     }
-
-	public static function getStyle($location,$brand=null)
+    
+    public static function getStyle($location, $brand = null)
     {
-	   $client = new \SoapClient(self::$brandwsdl);
-	   $client->__setSoapHeaders(self::$wsshead);
-	   $response   = $client->getBrand(array('locationNumber'=>$location,'brand'=>$brand));
-		
-	   return $response; 
+        $client = new \SoapClient(self::$brandwsdl);
+        $client->__setSoapHeaders(self::$wsshead);
+        $response = $client->getBrand(array(
+            'locationNumber' => $location,
+            'brand' => $brand
+        ));
+        
+        return $response;
     }
-
+    
     
     //************************************************
     //               Products Service
     //************************************************
     
-            
-
-
-    public static function getProdBrand($location,$product=null)
+    
+    
+    
+    public static function getProdBrand($location, $product = null)
     {
-	   $client = new \SoapClient(self::$productwsdl);
-	   $client->__setSoapHeaders(self::$wsshead);
-	   $response = $client->getBrand(array('locationNumber'=>$location,'productGroup'=>$product));
-		 
-	   return $response; 
+        $client = new \SoapClient(self::$productwsdl);
+        $client->__setSoapHeaders(self::$wsshead);
+        $response = $client->getBrand(array(
+            'locationNumber' => $location,
+            'productGroup' => $product
+        ));
+        
+        return $response;
     }
     
     public static function getProductByCriteria()
     {
-	    
-    } 
+        
+    }
     
     public static function getProductByKeyword()
     {
-	    
-    }   
+        
+    }
     
     //************************************************
     //               Order Service
@@ -122,13 +136,13 @@ class Atdconnect
     
     public static function placeOrder()
     {
-	    
-    } 
+        
+    }
     
     public static function previewOrder()
     {
-	    
-    } 
+        
+    }
     
     //************************************************
     //               Order Status Service
@@ -136,13 +150,13 @@ class Atdconnect
     
     public static function getOrderDetail()
     {
-	    
-    } 
+        
+    }
     
     public static function getOrderStatusByCriteria()
     {
-	    
-    } 
+        
+    }
     
     
     
