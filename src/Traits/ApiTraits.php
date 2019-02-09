d347b6928771c5aa6897dee66cb72eb39e1b7f2a
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2019-02-09 19:58:01 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- src/Traits/ApiTraits.php
+++ src/Traits/ApiTraits.php
@@ -6,40 +6,40 @@ use SoapClient;
 
 trait ApiTrait {
 
-	private $config; 
-	private $wsshead;
-	private $user;
-	private $pass;
-	private $client;
+        private $config; 
+        private $wsshead;
+        private $user;
+        private $pass;
+        private $client;
     
     
-    public  function apiCall($call,$query,$service)
-    {
-    	$config = include(realpath(dirname(__FILE__) . '/../config/config.php'));
+        public  function apiCall($call,$query,$service)
+        {
+                $config = include(realpath(dirname(__FILE__) . '/../config/config.php'));
     	
-    	$this->user = $config->user;
-    	$this->pass = $config->pass;
-    	$this->client = $config->client;
-    	$this->wsshead = $this->getWSSHeader();
+                $this->user = $config->user;
+                $this->pass = $config->pass;
+                $this->client = $config->client;
+                $this->wsshead = $this->getWSSHeader();
     	
         $client     = new \SoapClient($service);
         $client->__setSoapHeaders($this->wsshead);
-        $response 	= $client->$call($query);
+        $response = $client->$call($query);
         return $response;
-    }   
+        }   
     
-    public  function getWSSHeader()
-    {
+        public  function getWSSHeader()
+        {
         $xml = '
 		<wsse:Security SOAP-ENV:mustUnderstand="1" xmlns:wsse="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd">
-		    <wsse:UsernameToken atd:clientId="' . $this->client . '" xmlns:atd="http://api.atdconnect.com/atd">
-		        <wsse:Username>' . $this->user . '</wsse:Username>
-		        <wsse:Password Type="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordText">' . $this->pass . '</wsse:Password>
+		    <wsse:UsernameToken atd:clientId="' . $this->client.'" xmlns:atd="http://api.atdconnect.com/atd">
+		        <wsse:Username>' . $this->user.'</wsse:Username>
+		        <wsse:Password Type="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordText">' . $this->pass.'</wsse:Password>
 		    </wsse:UsernameToken>
 		</wsse:Security>
 		';
         
         return new \SoapHeader('http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd', 'Security', new \SoapVar($xml, XSD_ANYXML), true);
-    }
+        }
     
 }
