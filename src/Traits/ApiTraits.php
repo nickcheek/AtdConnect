21c629a57ef57940e7faac10be58a6f8d9e27aaf
Nicholas Cheek <nick@nicholascheek.com>
2019-02-09 13:53:55 -0600

    fixed trait variables

--- src/Traits/ApiTraits.php
+++ src/Traits/ApiTraits.php
@@ -8,25 +8,33 @@ trait ApiTrait {
 
 	private $config; 
 	private $wsshead;
+	private $user;
+	private $pass;
+	private $client;
     
-
+    
     public  function apiCall($call,$query,$service)
     {
     	$config = include(realpath(dirname(__FILE__) . '/../config/config.php'));
-    	$this->wsshead = $this->getWSSHeader($config->user, $config->pass, $config->client);
+    	
+    	$this->user = $config->user;
+    	$this->pass = $config->pass;
+    	$this->client = $config->client;
+    	$this->wsshead = $this->getWSSHeader();
+    	
         $client     = new \SoapClient($service);
         $client->__setSoapHeaders($this->wsshead);
         $response 	= $client->$call($query);
         return $response;
     }   
     
-    public  function getWSSHeader($user, $pass, $client)
+    public  function getWSSHeader()
     {
         $xml = '
 		<wsse:Security SOAP-ENV:mustUnderstand="1" xmlns:wsse="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd">
-		    <wsse:UsernameToken atd:clientId="' . $client . '" xmlns:atd="http://api.atdconnect.com/atd">
-		        <wsse:Username>' . $user . '</wsse:Username>
-		        <wsse:Password Type="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordText">' . $pass . '</wsse:Password>
+		    <wsse:UsernameToken atd:clientId="' . $this->client . '" xmlns:atd="http://api.atdconnect.com/atd">
+		        <wsse:Username>' . $this->user . '</wsse:Username>
+		        <wsse:Password Type="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordText">' . $this->pass . '</wsse:Password>
 		    </wsse:UsernameToken>
 		</wsse:Security>
 		';
